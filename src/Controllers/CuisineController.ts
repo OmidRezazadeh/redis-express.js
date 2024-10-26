234b654776082068f8efb1804fce20f1bb41e30c
orprogrammer <orprogrammer@gmail.com>
2024-10-26 22:11:32 +0330

    make  redis for  cuisines

--- src/Controllers/CuisineController.ts
+++ src/Controllers/CuisineController.ts
@@ -1,16 +1,28 @@
 import { Request, Response } from "express"; // Import Request and Response types from Express for type definitions
 import redis from "../configs/redis"; // Import the Redis instance from the configuration
 
-import { cuisinesKey } from "../Utils/key";
+import { restaurantKeyById,cuisinesKey,cuisineKey } from "../Utils/key";
 class CuisineController {
   async list(req: Request, res: Response): Promise<any> {
     try {
       const cuisines = await redis.smembers(cuisinesKey);
-      return res.status(200).json(cuisines);
+      return res.status(200).json({"cuisines":cuisines});
     
     } catch (err) {
       console.log(err);
     }
+  }
+  async getList(req: Request, res: Response): Promise<any>{
+        const cuisineId = req.params.id;
+        try {
+            const restaurantIds = await redis.smembers(cuisineKey(cuisineId));
+             const restaurants =await Promise.all(
+                restaurantIds.map((restaurantId)=> redis.hget(restaurantKeyById(restaurantId),"name"))
+             );
+             return res.status(200).json({"restaurants": restaurants});
+        }catch(err){
+
+        }
   }
 }
 
--- src/Routes/CuisinesRoute.ts
+++ src/Routes/CuisinesRoute.ts
@@ -2,5 +2,6 @@ import {cuisineController}  from '../Controllers/CuisineController';
 import express from 'express';
 
 const cuisineRoute = express.Router();
-cuisineRoute.get("/list",cuisineController.list.bind(cuisineController))
+cuisineRoute.get("/list/:id",cuisineController.list.bind(cuisineController))
+cuisineRoute.get("/get-list/:id",cuisineController.getList.bind(cuisineController))
 export default cuisineRoute;
